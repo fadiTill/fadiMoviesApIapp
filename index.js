e538dc478ad8b32bd640dc21206821e340a58499
Fadimata Tillman <fadi.tillman@gmail.com>
2022-02-12 17:34:53 -0800

    fetch with axios set up

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hi there!');
+
+const fetchData = async () => {
+    const response = await axios.get('http://www.omdbapi.com/',{
+        params: {
+apikey:'dfc61849',
+//search operation
+// s: 'avengers'
+
+//lookup
+i: 'tt0848228'
+        }
+    });
+console.log(response.data)
+};
+
+fetchData();
