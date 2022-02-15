a0862928ed955194698fc767ab30dc1f5cd2ae97
Fadimata Tillman <fadi.tillman@gmail.com>
2022-02-15 13:34:36 -0800

    replace timeoutId by debounce method

--- index.js
+++ index.js
@@ -20,19 +20,38 @@ console.log(response.data)
 
 const input = document.querySelector('input');
 //create timeout with delay to avoid api call on each key input 
-let timeoutId;
-const onInput = event => {
-    if (timeoutId){ 
-       clearTimeout(timeoutId) 
-    } 
-        timeoutId = setTimeout(() => {
-        fetchData(event.target.value);
-        }, 1000)
+const debounce = (func, delay = 1000) => {
+    let timeoutId;
+
+    return (...args) => {
+        if (timeoutId){ 
+            clearTimeout(timeoutId) 
+    };
+    timeoutId = setTimeout(() => {
+    func.apply(null, args);
+    // }, 1000)
+ }, delay)
+
+    }
+};
+
+// let timeoutId;
+const onInput = debounce(event => {
+    // const onInput = debounce(event => {
+
+    fetchData(event.target.value);
+
+    // if (timeoutId){ 
+    //    clearTimeout(timeoutId) 
+    // } 
+    //     timeoutId = setTimeout(() => {
+    //     fetchData(event.target.value);
+    //     }, 500)
     // fetchData(event.target.value)
 
 
-};
+});
 
-input.addEventListener('input',onInput)
-   
-// fetchData();
+input.addEventListener('input',debounce(onInput, 500));
+
+// fetchData()
