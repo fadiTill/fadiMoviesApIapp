1bab64842898deeb39b50c4eecf0a4b93b2e3041
Fadimata Tillman <fadi.tillman@gmail.com>
2022-02-15 12:52:41 -0800

    create timeout on input

--- index.js
+++ index.js
@@ -12,10 +12,27 @@ i: 'tt0848228'
     });
 console.log(response.data)
 };
+// const input = document.querySelector('input');
+// input.addEventListener('input',  (event) => {
+//     fetchData(event.target.value);
+
+// })
+
 const input = document.querySelector('input');
-input.addEventListener('input',  (event) => {
-    fetchData(event.target.value);
+//create timeout with delay to avoid api call on each key input 
+let timeoutId;
+const onInput = event => {
+    if (timeoutId){ 
+       clearTimeout(timeoutId) 
+    } 
+        timeoutId = setTimeout(() => {
+        fetchData(event.target.value);
+        }, 1000)
+    // fetchData(event.target.value)
 
-})
 
+};
+
+input.addEventListener('input',onInput)
+   
 // fetchData();
