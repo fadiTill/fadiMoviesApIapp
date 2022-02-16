caced4c1da4482d57d3eec3ede6e159050e91b31
Fadimata Tillman <fadi.tillman@gmail.com>
2022-02-16 11:41:03 -0800

    create utils file with debounce method

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ i: 'tt0848228'
     });
 console.log(response.data)
 };
-// const input = document.querySelector('input');
+//  const input = document.querySelector('input');
 // input.addEventListener('input',  (event) => {
 //     fetchData(event.target.value);
 
@@ -20,38 +20,51 @@ console.log(response.data)
 
 const input = document.querySelector('input');
 //create timeout with delay to avoid api call on each key input 
-const debounce = (func, delay = 1000) => {
-    let timeoutId;
+// const debounce = (func, delay = 1000) => {
+//     let timeoutId;
 
-    return (...args) => {
-        if (timeoutId){ 
-            clearTimeout(timeoutId) 
-    };
-    timeoutId = setTimeout(() => {
-    func.apply(null, args);
-    // }, 1000)
- }, delay)
+//     return (...args) => {
+//         if (timeoutId){ 
+//             clearTimeout(timeoutId) 
+//     };
+//     timeoutId = setTimeout(() => {
+//     func.apply(null, args);
+//     // }, 1000)
+//  }, delay)
 
-    }
-};
+//     }
+// };
 
-// let timeoutId;
+// // let timeoutId;
+// const onInput = debounce(event => {
+//     // const onInput = debounce(event => {
+
+//     fetchData(event.target.value);
+
+//     // if (timeoutId){ 
+//     //    clearTimeout(timeoutId) 
+//     // } 
+//     //     timeoutId = setTimeout(() => {
+//     //     fetchData(event.target.value);
+//     //     }, 500)
+//     // fetchData(event.target.value)
+
+
+// });
+
+// input.addEventListener('input',debounce(onInput, 500));
+
+// // fetchData()
+
+
 const onInput = debounce(event => {
-    // const onInput = debounce(event => {
+    
 
     fetchData(event.target.value);
 
-    // if (timeoutId){ 
-    //    clearTimeout(timeoutId) 
-    // } 
-    //     timeoutId = setTimeout(() => {
-    //     fetchData(event.target.value);
-    //     }, 500)
-    // fetchData(event.target.value)
+    
 
 
 });
 
 input.addEventListener('input',debounce(onInput, 500));
-
-// fetchData()
