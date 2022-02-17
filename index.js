f1690376caba3689646d57c0a1a8de887245e191
Fadimata Tillman <fadi.tillman@gmail.com>
2022-02-17 11:23:57 -0800

    update dropdown

--- index.js
+++ index.js
@@ -20,13 +20,16 @@ return response.data.Search;
 
 const root = document.querySelector('.autocomplete');
 root.innerHTML =`
-<label><b>Search for Movie</b>label>
-<input class="input"/>
+<label><b>Search For a Movie</b></label>
+<input class="input" />
 <div class="dropdown">
-<div class="dropdown-menu">
-</div class="dropdown-content results">
+  <div class="dropdown-menu">
+    <div class="dropdown-content results"></div>
+  </div>
 </div>
 `;
+
+
 
 
 const input = document.querySelector('input');
@@ -38,13 +41,15 @@ const onInput = async event => {
    const movies =  await fetchData(event.target.value);
    console.log(movies);
 
+   dropdown.classList.add('is-active');
    for(let movie of movies) { 
-       const div = document.createElement('div');
-
-       div.innerHTML =` <img src=${movie.Poster}/>
-                        <h1>${movie.Title}<h1/>
+       const option = document.createElement('a');
+       
+       option.classList.add('dropdown-item');
+       option.innerHTML =` <img src="${movie.Poster}"/>
+                        ${movie.Title}
        `;
-document.querySelector('#target').appendChild(div)
+resultsWrapper.appendChild(option);
    }
 };
 
