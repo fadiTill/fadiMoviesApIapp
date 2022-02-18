88544d5082c6526c5ccfae03d56e973a3c577f7f
Fadimata Tillman <fadi.tillman@gmail.com>
2022-02-18 12:56:17 -0800

    edit handling error images

--- index.js
+++ index.js
@@ -40,13 +40,15 @@ const resultsWrapper = document.querySelector('.results');
 const onInput = async event => {
    const movies =  await fetchData(event.target.value);
    console.log(movies);
-
+   
+   resultsWrapper.innerHTML = '';
    dropdown.classList.add('is-active');
    for(let movie of movies) { 
        const option = document.createElement('a');
+       const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
        
        option.classList.add('dropdown-item');
-       option.innerHTML =` <img src="${movie.Poster}"/>
+       option.innerHTML =` <img src="${imgSrc}"/>
                         ${movie.Title}
        `;
 resultsWrapper.appendChild(option);
