da51c17c8e4dce333ba467ccf1faefa24a1955e3
Fadimata Tillman <fadi.tillman@gmail.com>
2022-02-23 12:26:03 -0800

    reformate autocomplete code and render

--- index.js
+++ index.js
@@ -18,65 +18,86 @@ apikey:'dfc61849',
 return response.data.Search;
 };
 
-const root = document.querySelector('.autocomplete');
-root.innerHTML =`
-<label><b>Search For a Movie</b></label>
-<input class="input" />
-<div class="dropdown">
-  <div class="dropdown-menu">
-    <div class="dropdown-content results"></div>
-  </div>
-</div>
-`;
+
+createAutoComplete({
+  root: document.querySelector('.autocomplete')
+  });
+
+createAutoComplete({
+    root: document.querySelector('.autocomplete-two')
+    });
+
+ createAutoComplete({
+      root: document.querySelector('.autocomplete-three')
+      });
+
+// const root = document.querySelector('.autocomplete');
+// root.innerHTML =`
+// <label><b>Search For a Movie</b></label>
+// <input class="input" />
+// <div class="dropdown">
+//   <div class="dropdown-menu">
+//     <div class="dropdown-content results"></div>
+//   </div>
+// </div>
+// `;
 
 
 
 
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
+// const input = document.querySelector('input');
+// const dropdown = document.querySelector('.dropdown');
+// const resultsWrapper = document.querySelector('.results');
 
 
-const onInput = async event => {
-   const movies =  await fetchData(event.target.value);
-   console.log(movies);
+// const onInput = async event => {
+//    const movies =  await fetchData(event.target.value);
+//    console.log(movies);
 
-  if (!movies.length){
-    dropdown.classList.remove('is-active');
-    return;
+//   if (!movies.length){
+//     dropdown.classList.remove('is-active');
+//     return;
 
-  }
+//   }
    
-   resultsWrapper.innerHTML = '';
-   dropdown.classList.add('is-active');
-   for(let movie of movies) { 
-       const option = document.createElement('a');
-       const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+//    resultsWrapper.innerHTML = '';
+//    dropdown.classList.add('is-active');
+//    for(let movie of movies) { 
+//        const option = document.createElement('a');
+//        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
        
-       option.classList.add('dropdown-item');
-       option.innerHTML =` <img src="${imgSrc}"/>
-                        ${movie.Title}
-       `;
-       option.addEventListener('click', () => {
-      dropdown.classList.remove('is-active');
-      input.value = movie.Title;
-      onMovieSelect(movie);
-       })
+//        option.classList.add('dropdown-item');
+//        option.innerHTML =` <img src="${imgSrc}"/>
+//                         ${movie.Title}
+//        `;
+//        option.addEventListener('click', () => {
+//       dropdown.classList.remove('is-active');
+//       input.value = movie.Title;
+//       onMovieSelect(movie);
+//        })
 
-resultsWrapper.appendChild(option);
-   }
-};
+// resultsWrapper.appendChild(option);
+//    }
+// };
 
-input.addEventListener('input',debounce(onInput, 500));
+// input.addEventListener('input',debounce(onInput, 500));
 
-document.addEventListener('click', event =>{
-  // console.log(event.target)
+// document.addEventListener('click', event =>{
+//   // console.log(event.target)
 
-  if (!root.contains(event.target)){
-    dropdown.classList.remove('is-active');
+//   if (!root.contains(event.target)){
+//     dropdown.classList.remove('is-active');
 
-  }
-})
+//   }
+// })
+
+
+
+
+
+
+
+
 
 const  onMovieSelect = async (movie) => {
   // console.log(movie);
